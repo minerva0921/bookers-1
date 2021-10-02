cfaf19fc50d53b34e9f3b22ab9dcdff922a388e0
minerva0921 <121062hk@gmail.com>
2021-10-02 07:30:51 +0000

    [middle1002] meshiterro

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c2e5db4b39b6f8f29e843d06c1b26238a2a8b5a1ec57491e9d188fd82a035f01d3931d8016e4ceb843a0045de6bbd95eeed0ca097ca9c6ce687315cfedfe6395'
